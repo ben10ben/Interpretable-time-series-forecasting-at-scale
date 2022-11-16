44aa193fd6fbf704f918db67a19e1f0ab4f57970
ben10ben1 <brenedikt@gmail.com>
2022-11-16 13:07:48 +0100

    tft_update

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
-ELECTRICITY_PATH = ""
-RETAIL_PATH = ""
-WALMART_PATH = ""
-STOCKS_PATAH = ""
+from pathlib import Path
+
+WORKING_DIR = Path.cwd()
+
+CONFIG_DICT = {"datasets": 
+                        {"electricity" : WORKING_DIR / "data/electricity/",
+                        "retail"  : WORKING_DIR / "data/retail/",
+                        "walmart" : WORKING_DIR / "data/walmart/", 
+                        "stocks" : WORKING_DIR / "data/stocks/",
+                        },
+               "models":
+                        {"electricity" : WORKING_DIR / "models/electricity/",
+                         "retail"      : WORKING_DIR / "models/retail/",
+                         "walmart"     : WORKING_DIR / "models/walmart/",
+                         "stocks"      : WORKING_DIR / "models/stocks/",                    
+                        }
+           }
