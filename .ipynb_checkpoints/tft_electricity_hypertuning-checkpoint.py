a7b2c7282a08bbf9a91d7bb01f9337bd9ac6e610
ben10ben1 <brenedikt@gmail.com>
2023-03-04 15:08:05 +0100

    Lrf inder

--- .ipynb_checkpoints/tft_electricity_hypertuning-checkpoint.py
+++ .ipynb_checkpoints/tft_electricity_hypertuning-checkpoint.py
@@ -2,16 +2,9 @@ if __name__ == '__main__':
   print("Importing modules...")
 
   from torch.cuda import is_available
-  import pytorch_lightning as pl
-  import tensorboard as tb
-  import json
-  import time
-  import warnings
-  from torch import nn
-  from pytorch_lightning.accelerators import *
-  from torch.utils.tensorboard import SummaryWriter
+  #from torch.utils.tensorboard import SummaryWriter
   from lightning.pytorch.accelerators import find_usable_cuda_devices
-  from pytorch_lightning.loggers import TensorBoardLogger
+  #from pytorch_lightning.loggers import TensorBoardLogger
   from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
   from pytorch_forecasting.metrics import SMAPE, PoissonLoss, QuantileLoss
   from pytorch_forecasting.data.encoders import GroupNormalizer
@@ -22,7 +15,6 @@ if __name__ == '__main__':
   from config import *
   import pickle
   from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
-  from lightning.pytorch.accelerators import find_usable_cuda_devices
 
   print("Preparing dataset...") 
   
@@ -55,7 +47,7 @@ if __name__ == '__main__':
     dropout_range=(0.1, 0.3),
     trainer_kwargs=dict(limit_train_batches=40, max_epochs=20, log_every_n_steps=5, accelerator=accelerator, devices=devices),
     reduce_on_plateau_patience=4,
-    use_learning_rate_finder=True,  # use Optuna to find ideal learning rate or use in-built learning rate finder
+    use_learning_rate_finder=False,  # use Optuna to find ideal learning rate or use in-built learning rate finder
   )
 
   # save study results - also we can resume tuning at a later point in time
